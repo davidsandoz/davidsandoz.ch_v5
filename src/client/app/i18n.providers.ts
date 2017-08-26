629b89a31cd2fa5c5c3796656ff0b83724b31501
David Sandoz <david@sandoz.pro>
2017-08-27 00:07:23 +0200

    feat(i18n): use browser language by default

--- src/client/app/i18n.providers.ts
+++ src/client/app/i18n.providers.ts
@@ -7,8 +7,20 @@ export class TranslationProviders {
 
     // Define a way to retrieve the local information
     let locale: string;
+
+    // Retrieving locale based on the user browser
+    let firstBrowserLanguage = TranslationProviders.getFirstBrowserLanguage();
+
+    if (firstBrowserLanguage === 'fr-CH' || firstBrowserLanguage === 'en-US') {
+      locale = firstBrowserLanguage;
+    }
+    else {
+      locale = 'en-US';
+    }
+
+    // Retrieving locale from localStorage
     try {
-      locale = localStorage.getItem('locale') || 'en-US';
+      locale = localStorage.getItem('locale') || locale;
     } catch (e) {
       // Handle any errors (e.g. "SecurityError: The operation is insecure." if client blocks cookies.)
       locale = 'en-US';
@@ -32,6 +44,33 @@ export class TranslationProviders {
       xhr.onerror = () => reject(noProviders);
       xhr.send();
     });
+  };
+
+  private static getFirstBrowserLanguage(): string {
+    let nav = window.navigator;
+    let browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'];
+    let i;
+    let language;
+
+    // support for HTML 5.1 "navigator.languages"
+    if (Array.isArray(nav.languages)) {
+      for (i = 0; i < nav.languages.length; i++) {
+        language = nav.languages[i];
+        if (language && language.length) {
+          return language;
+        }
+      }
+    }
+
+    // support for other well known properties in browsers
+    for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+      language = (<any>nav)[browserLanguagePropertyKeys[i]];
+      if (language && language.length) {
+        return language;
+      }
+    }
+
+    return null;
   }
-};
+}
 
