5b80443cd1c376e3dc687ebfa324e553b197aa12
David Sandoz <david@sandoz.pro>
2017-08-27 00:18:00 +0200

    feat(i18n): make sure the locale is set correctly in the localStorage when the value comes from the browser

--- src/client/app/i18n.providers.ts
+++ src/client/app/i18n.providers.ts
@@ -2,50 +2,6 @@ import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 
 export class TranslationProviders {
 
-  public getTranslationFile = (): Promise<any> => {
-    let noProviders: Object[] = [];
-
-    // Define a way to retrieve the local information
-    let locale: string;
-
-    // Retrieving locale based on the user browser
-    let firstBrowserLanguage = TranslationProviders.getFirstBrowserLanguage();
-
-    if (firstBrowserLanguage === 'fr-CH' || firstBrowserLanguage === 'en-US') {
-      locale = firstBrowserLanguage;
-    }
-    else {
-      locale = 'en-US';
-    }
-
-    // Retrieving locale from localStorage
-    try {
-      locale = localStorage.getItem('locale') || locale;
-    } catch (e) {
-      // Handle any errors (e.g. "SecurityError: The operation is insecure." if client blocks cookies.)
-      locale = 'en-US';
-    }
-
-    // Set the directory to the translation files
-    let file: string = `../assets/locale/messages.${locale}.xlf`;
-
-    if (!locale || locale === 'en-US') return Promise.resolve(noProviders);
-
-    return new Promise(function (resolve, reject) {
-      let xhr = new XMLHttpRequest;
-      xhr.open('GET', file);
-      xhr.onload = (data: any) => resolve(
-        [
-          { provide: TRANSLATIONS, useValue: data.target.response },
-          { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
-          { provide: LOCALE_ID, useValue: locale }
-        ]
-      );
-      xhr.onerror = () => reject(noProviders);
-      xhr.send();
-    });
-  };
-
   private static getFirstBrowserLanguage(): string {
     let nav = window.navigator;
     let browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'];
@@ -71,6 +27,52 @@ export class TranslationProviders {
     }
 
     return null;
+  }
+
+  public getTranslationFile = (): Promise<any> => {
+    let noProviders: Object[] = [];
+
+    // Define a way to retrieve the local information
+    let locale: string;
+
+    // Retrieving locale based on the user browser
+    let firstBrowserLanguage = TranslationProviders.getFirstBrowserLanguage();
+
+    if (firstBrowserLanguage === 'fr-CH' || firstBrowserLanguage === 'en-US') {
+      locale = firstBrowserLanguage;
+    } else {
+      locale = 'en-US';
+    }
+
+    // Retrieving locale from localStorage
+    try {
+      locale = localStorage.getItem('locale') || locale;
+    } catch (e) {
+      // Handle any errors (e.g. "SecurityError: The operation is insecure." if client blocks cookies.)
+      locale = 'en-US';
+    }
+
+    // Set the locale in the localStorage in case we didn't get it from there.
+    localStorage.setItem('locale', locale);
+
+    // Set the directory to the translation files
+    let file: string = `../assets/locale/messages.${locale}.xlf`;
+
+    if (!locale || locale === 'en-US') return Promise.resolve(noProviders);
+
+    return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest;
+      xhr.open('GET', file);
+      xhr.onload = (data: any) => resolve(
+        [
+          { provide: TRANSLATIONS, useValue: data.target.response },
+          { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+          { provide: LOCALE_ID, useValue: locale }
+        ]
+      );
+      xhr.onerror = () => reject(noProviders);
+      xhr.send();
+    });
   }
 }
 
