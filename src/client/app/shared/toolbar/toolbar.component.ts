97477ab0f00ed535772e03502d34bf16ae8b03b8
David Sandoz <david@sandoz.pro>
2017-08-26 21:02:30 +0200

    feat(toolbar): add cover images

--- src/client/app/shared/toolbar/toolbar.component.ts
+++ src/client/app/shared/toolbar/toolbar.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LangSwitcherService} from "../lang-switcher/lang-switcher.service";
+import {NavigationStart, Router} from "@angular/router";
 
 /**
  * This class represents the toolbar component.
  */
 @Component({
   moduleId: module.id,
-  selector: 'sd-toolbar',
+  selector: 'ds-toolbar',
   templateUrl: 'toolbar.component.html',
   styleUrls: ['toolbar.component.css']
 })
-export class ToolbarComponent { }
+export class ToolbarComponent implements OnInit {
+  public locale: string;
+  public currentPageSlug: string;
+
+  public constructor (private router: Router) {
+    this.locale = LangSwitcherService.getLocale();
+  }
+
+  public selectLocale = (locale: string) => {
+    LangSwitcherService.setLocale(locale, this.currentPageSlug);
+  };
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.currentPageSlug = event.url.slice(1);
+      }
+    });
+  }
+}
 
