c82ccc605399337865159b2fd76c7cddab3ed61c
David Sandoz <david@sandoz.pro>
2017-08-26 21:12:14 +0200

    feat(footer): add footer

---
+++ src/client/app/shared/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {LangSwitcherService} from "../lang-switcher/lang-switcher.service";
+
+/**
+ * This class represents the navigation bar component.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'ds-footer',
+  templateUrl: 'footer.component.html',
+  styleUrls: ['footer.component.css'],
+})
+export class FooterComponent { }
--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {FooterComponent} from "./footer/footer.component";
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -12,8 +13,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [ToolbarComponent, NavbarComponent],
-  exports: [ToolbarComponent, NavbarComponent,
+  declarations: [ToolbarComponent, NavbarComponent, FooterComponent],
+  exports: [ToolbarComponent, NavbarComponent, FooterComponent,
     CommonModule, FormsModule, RouterModule]
 })
 export class SharedModule {
